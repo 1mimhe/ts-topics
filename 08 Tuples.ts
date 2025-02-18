b873aff854e336664a4cfc93ac8562fd9fd6d9f9
Mohammad Hosseini <mimhe1381@gmail.com>
2025-02-18 19:20:21 +0330

    tuples

---
+++ 08 Tuples.ts
@@ -0,0 +1,16 @@
+// a arrays with fixed element orders.
+// each element represents something.
+
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+// Type alias
+type Drink = [string, boolean, number];
+
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
